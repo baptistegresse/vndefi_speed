24e671a6fbf22726821026f973b5c1fd750dc730
baptiste gresse <baptiste5gresse@gmail.com>
2025-12-24 02:00:17 +0700

    Update Next.js configuration to include additional Prisma output file tracing and adjust build scripts for improved efficiency. Add seeding command in Prisma configuration for better integration.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    "/*": ["./app/generated/prisma/**/*"],
+    "/*": [
+      "./app/generated/prisma/*.node",
+      "./app/generated/prisma/*.so.node",
+      "./node_modules/.prisma/client/*.node",
+    ],
   },
+  serverExternalPackages: ["@prisma/client"],
 };
 
 export default nextConfig;
